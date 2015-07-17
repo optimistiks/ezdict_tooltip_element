999e1e366cb2ca728a0a6c6b807618b15309496d
Potapov Max <mp@multiship.ru>
2015-07-17 18:50:11 +0300

    add some api to element

--- ezdict_tooltip_element/ezdict_tooltip_element.js
+++ ezdict_tooltip_element/ezdict_tooltip_element.js
@@ -1,41 +1,8 @@
 Polymer({
   is: 'ezdict-tooltip-element',
   properties: {
-
-    /**
-     * `fancy` indicates that the element should don a monocle and tophat,
-     * while checking its pocket watch.
-     */
-    fancy: Boolean,
-
-    /**
-     * Describes the author of the element, but is really just an excuse to
-     * show off JSDoc annotations.
-     *
-     * @type {{name: string, image: string}}
-     */
-    author: {
-      type: Object,
-      // Use `value` to provides a default value for a property, by setting it
-      // on your element's prototype.
-      //
-      // If you provide a function, as we do here, Polymer will call that
-      // _per element instance_.
-      //
-      // We do that to ensure that each element gets its own copy of the
-      // value, rather than having it shared across all instances (via the
-      // prototype).
-      value: function () {
-        return {
-          name: 'test',
-          image: 'test',
-        };
-      }
-    },
-
+    translation: String
   },
-
-  // Element Lifecycle
 
   ready: function () {
     // `ready` is called after all elements have been configured, but
@@ -62,34 +29,23 @@ Polymer({
     // Use this to clean up anything you did in `attached`.
   },
 
-  // Element Behavior
-
-  /**
-   * The `ezdict-tooltip-element-lasers` event is fired whenever `fireLasers` is called.
-   *
-   * @event ezdict-tooltip-element-lasers
-   * @detail {{sound: String}}
-   */
-
-  /**
-   * Sometimes it's just nice to say hi.
-   *
-   * @param {string} greeting A positive greeting.
-   * @return {string} The full greeting.
-   */
-  sayHello: function (greeting) {
-    var response = greeting || 'Hello World!';
-    return 'ezdict-tooltip-element says, ' + response;
+  setTranslation: function (translation) {
+    this.translation = translation;
   },
 
-  /**
-   * Attempts to destroy this element's enemies with an any beam of light!
-   *
-   * Or, at least, dispatches an event in the vain hope that someone else will
-   * do the zapping.
-   */
-  fireLasers: function () {
-    this.fire('ezdict-tooltip-element-lasers', {sound: 'Pew pew!'});
-  }
+  setTop: function (top) {
+    this.$$('#sticker').style.top = top;
+  },
 
+  setLeft: function (left) {
+    this.$$('#sticker').style.left = left;
+  },
+
+  hide: function (left) {
+    this.$$('#sticker').style.display = 'none';
+  },
+
+  show: function (left) {
+    this.$$('#sticker').style.display = 'block';
+  }
 });
